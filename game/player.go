10a22c672aaf77078ac4a0f7a58526a70f7b25c0
Ahmet Affan <ahmet.affan.ebcioglu@outlook.com>
2024-03-19 22:04:53 +0300

    ehh

---
+++ game/player.go
@@ -0,0 +1,14 @@
+package game
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+type Player struct {
+}
+
+func (p *Player) Update() error {
+	return nil
+}
+
+func (p *Player) Draw(screen *ebiten.Image) {
+
+}
